2b5c5922192942f569dae2ed2c980ef6757d3bbe
xpakx <55586470+xpakx@users.noreply.github.com>
2022-01-31 15:08:49 +0100

    fix(backend): fix checking word for duplications

--- backend/src/main/java/io/github/xpakx/wordle/service/PuzzleService.java
+++ backend/src/main/java/io/github/xpakx/wordle/service/PuzzleService.java
@@ -11,7 +11,10 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Service
 @AllArgsConstructor
@@ -30,11 +33,26 @@ public class PuzzleService {
         }
         WordResponse response = new WordResponse();
         response.setPositions(new ArrayList<>());
+
+        List<Boolean> matches = IntStream.range(0, wordSplit.length)
+                .mapToObj((i) -> answerSplit[i].equals(wordSplit[i]))
+                .collect(Collectors.toList());
+
+        HashMap<String, Integer> letterFrequency = new HashMap<>();
+        for(String letter : wordSplit) {
+            if(letterFrequency.containsKey(letter)) {
+                letterFrequency.put(letter, letterFrequency.get(letter)+1);
+            } else {
+                letterFrequency.put(letter, 1);
+            }
+        }
+
         for(int i = 0; i<wordSplit.length; i++) {
-           if(answerSplit[i].equals(wordSplit[i])) {
+           if(matches.get(i)) {
                response.getPositions().add(2);
-           } else if(contains(wordSplit, answerSplit[i])) {
+           } else if(letterFrequency.get(answerSplit[i])>0 && contains(wordSplit, answerSplit[i], matches)) {
                response.getPositions().add(1);
+               letterFrequency.put(answerSplit[1], letterFrequency.get(answerSplit[1])-1);
            } else {
                response.getPositions().add(0);
            }
@@ -42,7 +60,12 @@ public class PuzzleService {
         return response;
     }
 
-    private boolean contains(String[] wordSplit, String answerSplit) {
-        return Arrays.asList(wordSplit).contains(answerSplit);
+    private boolean contains(String[] wordSplit, String letter, List<Boolean> matches) {
+        for(int i=0; i< wordSplit.length; i++) {
+            if(!matches.get(i) && letter.equals(wordSplit[i])) {
+                return true;
+            }
+        }
+        return false;
     }
 }
